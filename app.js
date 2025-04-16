1dbfcd288efa2dc3f5d98271a8e6a0a58b6498be
Fars <falk@infosys.com>
2025-04-15 19:36:56 -0500

    task 1,2 completed

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+function r_e(id) {
+  return document.querySelector(`#${id}`);
+}
+
 const teams = [
   {
     name: "Real Madrid",
@@ -52,7 +56,139 @@ const teams = [
 
 teams.forEach((team) => {
   db.collection("teams")
-    .add(team)
-    .then((docRef) => console.log("Added:", docRef.id))
-    .catch((error) => console.error("Error:", error));
+    .where("name", "==", team.name)
+    .get()
+    .then((t) => {
+      if (t.empty) {
+        db.collection("teams")
+          .add(team)
+          .then((docRef) => console.log("Added:", docRef.id))
+          .catch((error) => console.error("Error:", error));
+      } else {
+        console.log(`${team.name} already exists`);
+      }
+    });
 });
+
+db.collection("teams")
+  .where("country", "==", "Spain")
+  .get()
+  .then((data) => {
+    mydocs = data.docs;
+    let html = ``;
+
+    mydocs.forEach((d) => {
+      let temp = d.data();
+      html += `<p>${temp.name}</p>`;
+    });
+    r_e("spainteams").innerHTML = html;
+  });
+
+db.collection("teams")
+  .where("country", "==", "Spain")
+  .where("city", "==", "Madrid")
+  .get()
+  .then((data) => {
+    mydocs = data.docs;
+    let html = ``;
+
+    mydocs.forEach((d) => {
+      let temp = d.data();
+      html += `<p>${temp.name}</p>`;
+    });
+    r_e("madrid").innerHTML = html;
+  });
+db.collection("teams")
+  .where("city", "==", "Not applicable")
+  .get()
+  .then((data) => {
+    mydocs = data.docs;
+    let html = ``;
+
+    mydocs.forEach((d) => {
+      let temp = d.data();
+      html += `<p>${temp.name}</p>`;
+    });
+    r_e("national").innerHTML = html;
+  });
+
+db.collection("teams")
+  .where("country", "!=", "Spain")
+  .get()
+  .then((data) => {
+    mydocs = data.docs;
+    let html = ``;
+
+    mydocs.forEach((d) => {
+      let temp = d.data();
+      html += `<p>${temp.name}</p>`;
+    });
+    r_e("nspain").innerHTML = html;
+  });
+
+db.collection("teams")
+  .where("country", "not-in", ["Spain", "England"])
+  .get()
+  .then((data) => {
+    mydocs = data.docs;
+    let html = ``;
+
+    mydocs.forEach((d) => {
+      let temp = d.data();
+      html += `<p>${temp.name}</p>`;
+    });
+    r_e("nspen").innerHTML = html;
+  });
+db.collection("teams")
+  .where("country", "==", "Spain")
+  .where("fans_mil", ">", 700)
+  .get()
+  .then((data) => {
+    mydocs = data.docs;
+    let html = ``;
+
+    mydocs.forEach((d) => {
+      let temp = d.data();
+      html += `<p>${temp.name}</p>`;
+    });
+    r_e("sp700").innerHTML = html;
+  });
+db.collection("teams")
+  .where("fans_mil", ">", 500)
+  .where("fans_mil", "<", 600)
+  .get()
+  .then((data) => {
+    mydocs = data.docs;
+    let html = ``;
+
+    mydocs.forEach((d) => {
+      let temp = d.data();
+      html += `<p>${temp.name}</p>`;
+    });
+    r_e("q7").innerHTML = html;
+  });
+db.collection("teams")
+  .where("topScorers", "array-contains", "Ronaldo")
+  .get()
+  .then((data) => {
+    mydocs = data.docs;
+    let html = ``;
+
+    mydocs.forEach((d) => {
+      let temp = d.data();
+      html += `<p>${temp.name}</p>`;
+    });
+    r_e("ron").innerHTML = html;
+  });
+db.collection("teams")
+  .where("topScorers", "array-contains-any", ["Messi", "Ronaldo", "Maradona"])
+  .get()
+  .then((data) => {
+    mydocs = data.docs;
+    let html = ``;
+    mydocs.forEach((d) => {
+      let temp = d.data();
+      html += `<p>${temp.name}</p>`;
+    });
+    r_e("rmm").innerHTML = html;
+  });
